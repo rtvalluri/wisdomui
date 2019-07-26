a60fc8fd63b887dfa4875bba68e0fff86d2793aa
rtvalluri <valluriraviteja@gmail.com>
2019-07-26 17:55:28 +0530

    students module created with working route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeRoutingModule } from './home/home-routing.module';
+import { StudentsRoutingModule } from './students/students-routing.module';
 
 const routes: Routes = [{
   path: '',
   redirectTo: 'home',
   pathMatch: 'full'
 }, {
+  path: 'home',
+  loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+}, {
+  path: 'students',
+  loadChildren: () => import('./students/students.module').then(m => m.StudentsModule)
+}, {
   path: '**',
   redirectTo: '',
   pathMatch: 'full'
-}, {
-  path: 'home',
-  loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
 }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), HomeRoutingModule],
+  imports: [
+    RouterModule.forRoot(routes),
+    HomeRoutingModule,
+    StudentsRoutingModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 import { FormsModule } from '@angular/forms';
+import { StudentsModule } from './students/students.module';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { FormsModule } from '@angular/forms';
     MatToolbarModule,
     MatIconModule,
     MatListModule,
-    HomeModule
+    HomeModule,
+    StudentsModule
   ],
   exports: [
   ],
