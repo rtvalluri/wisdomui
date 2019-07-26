4e74341f87dc33fadcbadc3c48814cae742e8923
rtvalluri <valluriraviteja@gmail.com>
2019-07-26 17:15:09 +0530

    Basic layout with sidenav toggle created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeRoutingModule } from './home/home-routing.module';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  redirectTo: 'home',
+  pathMatch: 'full'
+}, {
+  path: '**',
+  redirectTo: '',
+  pathMatch: 'full'
+}, {
+  path: 'home',
+  loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), HomeRoutingModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+  MatSidenavModule,
+  MatCheckboxModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +20,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    HomeModule
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
